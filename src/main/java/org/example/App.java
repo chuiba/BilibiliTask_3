4ab908584892ea7890db3f5c1006033ea9de4dd8
srcrs <srcrs@foxmail.com>
2020-10-05 17:08:20 +0800

    改变Sec变量

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,10 +8,9 @@ public class App
 {
     public static void main( String[] args ) throws Exception {
         Cookie cookie = new Cookie();
-        JSONObject jsonObject = JSON.parseObject(args[0]);
-        cookie.setBili_jct(jsonObject.getString("bili_jct"));
-        cookie.setDedeUserID(jsonObject.getString("DedeUserID"));
-        cookie.setSESSDATA(jsonObject.getString("SESSDATA"));
+        cookie.setBili_jct(args[0]);
+        cookie.setDedeUserID(args[1]);
+        cookie.setSESSDATA(args[2]);
         // 执行获取经验
         new DailyTasks();
     }
