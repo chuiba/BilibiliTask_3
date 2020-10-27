52671f1b0766c162f06586a621f2c3c8bf65be78
srcrs <srcrs@foxmail.com>
2020-10-27 12:34:48 +0800

    修复任务执行顺序错误

--- src/main/java/top/srcrs/bilibili/BiliStart.java
+++ src/main/java/top/srcrs/bilibili/BiliStart.java
@@ -10,6 +10,9 @@ import org.slf4j.LoggerFactory;
 import top.srcrs.bilibili.util.SendServer;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * 启动类，程序运行开始的地方
@@ -22,12 +25,20 @@ public class BiliStart {
     private static Data data = Data.getInstance();
 
     public static void main(String[] args) {
+        /**
+         * 存储所有 class 全路径名
+         * 因为测试的时候发现，在 windows 中是按照字典排序的
+         * 但是在 Linux 中并不是字典排序我就很迷茫
+         * 因为部分任务是需要有顺序的去执行
+         */
+        final List<String> list = new ArrayList<>();
         if(args.length==0){
             LOGGER.error("请在Github Secrets中添加你的Cookie信息");
         }
         data.setCookie(args[0],args[1],args[2]);
         /** 读取yml文件配置信息 */
         ReadConfig.transformation("/config.yml");
+        /** 如果用户账户有效 */
         if(check()){
             LOGGER.info("用户名: {}",data.getUname());
             LOGGER.info("硬币: {}",data.getMoney());
@@ -36,17 +47,25 @@ public class BiliStart {
                 @Override
                 public void dealClass(Class<?> klass) {
                     try{
-                        Object object = klass.newInstance();
-                        Class clazz = object.getClass();
-                        Method method = clazz.getMethod("run",null);
-                        method.invoke(object);
+                        list.add(klass.getName());
                     } catch (Exception e){
-                        LOGGER.error("反射获取对象错误 -- "+e);
+                        LOGGER.error("扫描class目录出错 -- "+e);
                     }
                 }
             };
             /** 动态执行task包下的所有java代码 */
             pack.scannerPackage("top.srcrs.bilibili.task");
+            Collections.sort(list);
+            for(String s : list){
+                try{
+                    Object object = Class.forName(s).newInstance();
+                    Class clazz = object.getClass();
+                    Method method = clazz.getMethod("run",null);
+                    method.invoke(object);
+                } catch (Exception e){
+                    LOGGER.error("反射获取对象错误 -- "+e);
+                }
+            }
             LOGGER.info("本次任务运行完毕。");
             /** 如果用户填了server酱的SCKEY就会执行 */
             if(args.length==4){
